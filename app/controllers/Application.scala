d779e76761db98c5d7eb4333366a2a37d48dd286
Igor Rumiha <igorrumiha@gmail.com>
2015-04-22 23:58:42 +0200

    Working TodoMVC.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,56 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import play.api.data._
+import play.api.data.Forms._
+
+import models.Todo
+
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index())
+  var todos: List[Todo]= List()
+
+  val todoForm = Form(
+    mapping(
+      "todo" -> text,
+      "done" -> default(boolean, false)
+    )(Todo.apply)(Todo.unapply)
+  )
+
+  def index = Action { request =>
+    Ok(views.html.index(todos, request.flash.get("errors").getOrElse("")))
   }
 
+  def createTodo = Action { implicit request =>
+    todoForm.bindFromRequest().fold(
+      frm => {
+        Redirect(routes.Application.index()).flashing(
+          "errors" -> frm.errors.map(_.message).mkString(",")
+        )
+      },
+      newTodo => {
+        todos = todos :+ newTodo
+        Redirect(routes.Application.index())
+      }
+    )
+  }
+
+  def removeTodo(idx: Int) = Action {
+    todos = todos.zipWithIndex.filter{case(_, i) => i != idx}.map(_._1)
+    Redirect(routes.Application.index())
+  }
+
+  def flipTodoDone(idx: Int) = Action {
+    todos = todos.zipWithIndex.map{
+      case (t, i) =>
+        if (i == idx) {
+          if (t.done)
+            t.copy(done = false)
+          else
+            t.copy(done = true)
+        }
+        else t
+    }
+    Redirect(routes.Application.index())
+  }
 }
---
+++ app/models/Todo.scala
@@ -0,0 +1,3 @@
+package models
+
+case class Todo(todo: String, done: Boolean)
